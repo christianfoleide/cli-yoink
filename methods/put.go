100494ff0e851ea64244e9a221a1101156ce6aa9
christianfoleide <christian.foleide@gmail.com>
2020-07-21 00:10:24 +0200

    implement http put client

--- methods/put.go
+++ methods/put.go
@@ -1,6 +1,42 @@
 package methods
 
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"yo/util"
+)
+
 func Put(dest string, filename string) ([]byte, error) {
-	bytes := []byte{'P','U','T'}
-	return bytes, nil
+
+	file, err := util.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	reqBody := bytes.NewBuffer(file)
+
+	client := http.Client{}
+
+	req, err := http.NewRequest(http.MethodPut, dest, reqBody)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	body := resp.Body
+	defer body.Close()
+
+	respBytes, err := ioutil.ReadAll(body)
+	if err != nil {
+		return nil, err
+	}
+
+	return respBytes, nil
+
 }
