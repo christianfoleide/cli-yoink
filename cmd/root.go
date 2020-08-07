0b321da49d3ddedab6e8381b7038d06c3fc8bfbe
christianfoleide <christian.foleide@gmail.com>
2020-08-07 15:27:25 +0200

    refactor rootcommand

--- cmd/root.go
+++ cmd/root.go
@@ -2,85 +2,36 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/spf13/cobra"
-	"net/http"
-	"os"
-	"strings"
-	"github.com/christianfoleide/yoink/methods"
 	"github.com/christianfoleide/yoink/util"
+	"os"
+	"github.com/christianfoleide/yoink/client"
+	"github.com/spf13/cobra"
 )
 
 var (
-
 	aliases = []string{"yoink"}
 
 	Pretty bool
 	Method bool
 
 	rootCmd = &cobra.Command{
-		Use: "yo",
+		Use:     "yo",
 		Aliases: aliases,
-		Short: "A network CLI for get, post and put requests to an API",
-		Long: "A network CLI for GET, POST, and PUT requests to an API where you can specify json files to send as test data. Defaults to GET",
+		Short:   "A network CLI for get, post and put requests to an API",
+		Long:    "A network CLI for GET, POST, and PUT requests to an API where you can specify json files to send as test data. Defaults to GET",
 		Args: func(cmd *cobra.Command, args []string) error {
 
-			if !Method { //first and only argument is URL
-				return util.ValidateUrl(args[0])
-			}
-			//first argument is request-method, second argument is URL
-			return util.ValidateArgs(args)
+			return nil
 		},
 		Run: func(cmd *cobra.Command, args []string) {
 
-			if !Method { //default get
-				dest := args[0]
-				result, err := methods.Get(dest)
-
-				if err != nil {
-					fmt.Println(err)
-					return
-				}
-
-				if Pretty {
-					util.PrettyPrint(result)
-				} else {
-					fmt.Println(string(result))
-				}
-				return
+			c := client.NewClient(args[0], args[1])
+			b, err := c.DoRequest()
+			if err != nil {
+				fmt.Println(err)
 			}
-			//args are already validated
 
-			method := strings.ToUpper(args[0])
-			dest := args[1]
-			file := args[2]
-
-			switch method {
-			case http.MethodPost:
-				result, err := methods.Post(dest, file)
-				if err != nil {
-					fmt.Println(err)
-					return
-				}
-				if Pretty {
-					util.PrettyPrint(result)
-					return
-				}
-				fmt.Println(string(result))
-				return
-				
-			case http.MethodPut:
-				result, err := methods.Put(dest, file)
-				if err != nil {
-					fmt.Println(err)
-					return
-				}
-				if Pretty {
-					util.PrettyPrint(result)
-					return
-				}
-				fmt.Println(string(result))
-				return
-			}
+			util.PrettyPrint(b)
 		},
 	}
 )
